2f2001837f2c2e713bf58f9be1865a096d018318
Anna <annakul@students.zcu.cz>
2024-03-04 14:08:24 +0100

    dalsi upravy

--- ProjektEvidence/Models/Evidence.cs
+++ ProjektEvidence/Models/Evidence.cs
@@ -2,7 +2,15 @@
 {
     public class Evidence
     {
-        
+        public List<Polozka> Polozky { get; set; } = new List<Polozka>();
+        public Polozka Polozka { get; set; } = new Polozka();
 
+        public void Pridat()
+        {
+            //Polozky.Add(Polozka);
+            //Polozka = new Polozka();
+
+            Polozky.Add(new Polozka(datum: Polozka.Datum, vynosy: Polozka.Vynosy, naklady: Polozka.Naklady));
+        }
     }
 }
--- ProjektEvidence/Models/Polozka.cs
+++ ProjektEvidence/Models/Polozka.cs
@@ -2,6 +2,19 @@
 {
     public class Polozka
     {
+        //je to construktor, musi se jmenovat stejne jako trida
+        public Polozka()
+        {
+            Datum = DateOnly.FromDateTime(DateTime.Now);
+        }
+
+        public Polozka(DateOnly datum, double vynosy, double naklady)
+        {
+            Datum = datum;
+            Vynosy = vynosy;
+            Naklady = naklady;
+        }
+
         //v poli v C# nejde menit velikost. Proto vyuzijeme seznam (pomoci tridy jako objekt)
         public DateOnly Datum { get; set; }
         private double vynosy;
@@ -31,15 +44,15 @@
             get { return naklady; }
             set
             {
-                if (vynosy != value)
+                if (naklady != value)
                 {
                     if (value < 0)
                     {
-                        vynosy = 0;
+                        naklady = Math.Abs(value);
                     }
                     else
                     {
-                        vynosy = value;
+                        naklady = value;
                     }
                 }
             }
