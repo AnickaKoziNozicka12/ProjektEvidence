79128b835818dd29c6f79d32bf1e95cf05e3c2fd
Anna <annakul@students.zcu.cz>
2024-03-18 14:34:12 +0100

    EVIDENCE pokrok

--- ProjektEvidence/Models/Evidence.cs
+++ ProjektEvidence/Models/Evidence.cs
@@ -1,11 +1,24 @@
-﻿namespace ProjektEvidence.Models
+﻿using Microsoft.JSInterop;
+using System.Globalization;
+using System.Xml.Serialization;
+
+namespace ProjektEvidence.Models
 {
     public class Evidence
     {
+        public Evidence(IJSRuntime js)
+        {
+            JS = js;
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("cs-CZ");
+        }
+        private IJSRuntime JS { get; set; }
+
         public List<Polozka> Polozky { get; set; } = new List<Polozka>();
         public Polozka Polozka { get; set; } = new Polozka();
 
         public string Vypis { get; set; } = "";
+
+        public bool IsEditace { get; set; } = false;
 
         public void Pridat()
         {
@@ -23,6 +36,73 @@
         public void ZobrazAll()
         {
             Vypis = $"Jednotlivé záznamy: <br>" + string.Join("<br>", Polozky);
+        }
+
+        public void Statistiky()
+        {
+            Vypis = "";
+            Vypis += $"Minimum: {Minimum()}<br>";
+            Vypis += $"Maximum: {Maximum()}<br>";
+            Vypis += $"Průměr: {Prumer()}";
+        }
+
+        private double Maximum()
+        {
+            if (Polozky.Count == 0)
+            {
+                return double.NaN;
+            }
+            else
+            {
+                return Polozky.Max(x => x.Zisk);
+            }
+        }
+
+        private double Minimum()
+        {
+            if (Polozky.Count == 0)
+            {
+                return double.NaN;
+            }
+            else
+            {
+                return Polozky.Min(x=>x.Zisk);
+            }
+        }
+        private double Prumer()
+        {
+            if (Polozky.Count == 0)
+            {
+                return double.NaN;
+            }
+            else
+            {
+                return Polozky.Average(x => x.Zisk);
+            }
+        }
+
+        public async Task SmazatZaznamAsync(Models.Polozka polozka)
+        {
+            //JavaScript.InvokeVoidAsync("alert", "Hlaska"); //zavorky: prvni je fce z JS a pak funkce
+            var zprava = $"Chcete odebrat {polozka.Datum} - Zisk {polozka.Zisk}?";
+
+            if (await JS.InvokeAsync<bool>("confirm", zprava))
+            {
+                Polozky.Remove(polozka);
+            }
+        }
+
+        public void Edituj(Models.Polozka polozka)
+        {
+            Polozka = polozka;
+
+            IsEditace = true;
+        }
+
+        public void UkonciEditaci()
+        {
+            IsEditace = false;
+            Polozka = new Polozka();
         }
     }
 }
--- ProjektEvidence/Pages/Evidence.razor.cs
+++ ProjektEvidence/Pages/Evidence.razor.cs
@@ -1,7 +1,20 @@
-﻿namespace ProjektEvidence.Pages
+﻿using Microsoft.JSInterop;
+using ProjektEvidence.Models;
+
+namespace ProjektEvidence.Pages
 {
     public partial class Evidence
     {
-
+        //do zavorky ktera polozka byla vybrana
+        //private async Task SmazatZaznamAsync(Models.Polozka polozka)
+        //{
+        //    //JavaScript.InvokeVoidAsync("alert", "Hlaska"); //zavorky: prvni je fce z JS a pak funkce
+        //    var zprava = $"Chcete odebrat {polozka.Datum} - Zisk {polozka.Zisk}?";
+            
+        //    if (await JavaScript.InvokeAsync<bool>("confirm", zprava))
+        //    {
+        //        Evid.Polozky.Remove(polozka);
+        //    }
+        //}
     }
 }
